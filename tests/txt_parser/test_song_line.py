12ce1de4f75eba6642fc34825fc897242b687894
dipzza <dipzza@disroot.org>
2022-08-27 17:51:04 +0200

    :recycle::white_check_mark: Refactor SongLine child classes according to #47
    + Update related uses and tests

--- tests/txt_parser/test_song_line.py
+++ tests/txt_parser/test_song_line.py
@@ -31,14 +31,6 @@ def test_player_change_str():
     assert str(change) == PLAYER_STR_MODEL.format(c.PLAYER)
 
 
-def test_player_change_eq_true():
-    assert sl.PlayerChange(c.PLAYER) == sl.PlayerChange(c.PLAYER)
-
-
-def test_player_change_eq_false():
-    assert sl.PlayerChange(c.PLAYER) != sl.PlayerChange(PLAYER_2)
-
-
 def test_note_valid():
     note = sl.Note(sl.NoteType.NORMAL, c.START_BEAT, c.LENGTH, c.MIDI, c.WORD)
 
@@ -59,22 +51,6 @@ def test_note_str():
 
     assert str(note) == NOTE_STR_MODEL.format(
         note.type.value, note.start_beat, note.length, note.pitch, note.text)
-
-
-def test_note_eq_true():
-    note_a = sl.Note(sl.NoteType.RAP, c.START_BEAT, c.LENGTH, c.MIDI, c.WORD)
-    note_b = sl.Note(sl.NoteType.RAP, c.START_BEAT, c.LENGTH, c.MIDI, c.WORD)
-
-    assert note_a == note_b
-
-
-def test_note_eq_false():
-    note_a = sl.Note(sl.NoteType.NORMAL, c.START_BEAT,
-                     c.LENGTH, c.MIDI, c.WORD)
-    note_b = sl.Note(sl.NoteType.GOLDEN, c.START_BEAT,
-                     c.LENGTH, c.MIDI, c.WORD)
-
-    assert note_a != note_b
 
 
 def test_phrase_end_valid():
@@ -100,17 +76,3 @@ def test_phrase_end_interval_str():
     phrase_end_str = PHRASE_STR_MODEL_2.format(c.START_BEAT, c.END_BEAT)
 
     assert str(phrase_end) == phrase_end_str
-
-
-def test_phrase_end_eq_true():
-    phrase_end_a = sl.PhraseEnd(c.START_BEAT, c.END_BEAT)
-    phrase_end_b = sl.PhraseEnd(c.START_BEAT, c.END_BEAT)
-
-    assert phrase_end_a == phrase_end_b
-
-
-def test_phrase_end_eq_false():
-    phrase_end_a = sl.PhraseEnd(c.START_BEAT, c.END_BEAT)
-    phrase_end_b = sl.PhraseEnd(c.START_BEAT, END_BEAT_2)
-
-    assert phrase_end_a != phrase_end_b
