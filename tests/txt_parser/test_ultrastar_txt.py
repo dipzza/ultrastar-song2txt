c3c141683a4584723d883011ae274a8f08b9672d
dipzza <dipzza@disroot.org>
2022-08-27 17:52:12 +0200

    :recycle::white_check_mark: Refactor UltraStar class according to #48
    + Update related tests

--- tests/txt_parser/test_ultrastar_txt.py
+++ tests/txt_parser/test_ultrastar_txt.py
@@ -1,26 +1,11 @@
-from copy import copy
-
-from song2txt.txt_parser.ultrastar_txt import UltraStarTXT
+from song2txt.txt_parser.song_line import Note, NoteType
+import tests.conftest as c
 
 
 def test_str(us_txt, us_txt_text):
     assert str(us_txt) == us_txt_text
 
 
-def test_eq_true(metadata, song_lines):
-    us_txt = UltraStarTXT(metadata, song_lines)
-    metadata_copy = copy(metadata)
-    song_lines_copy = copy(song_lines)
-    us_txt_copy = UltraStarTXT(metadata_copy, song_lines_copy)
-
-    assert us_txt == us_txt_copy
-
-
-def test_eq_false(metadata, song_lines):
-    us_txt = UltraStarTXT(metadata, song_lines)
-    metadata_copy = copy(metadata)
-    song_lines_copy = copy(song_lines)
-    us_txt_copy = UltraStarTXT(metadata_copy, song_lines_copy)
-    us_txt_copy.metadata.relative = not us_txt.metadata.relative
-
-    assert us_txt != us_txt_copy
+def test_get_notes(us_txt):
+    expected = [Note(NoteType.NORMAL, c.START_BEAT, c.LENGTH, c.MIDI, c.WORD)]
+    assert us_txt.get_notes() == expected
